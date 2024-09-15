c7d11ae6d9b7faadbd8c7d79ce08f6c81dd14d3d
kovacs7 <asishkumarbehera.official@gmail.com>
2024-09-15 13:44:12 +0530

    Lessons tab done.

--- src/app/schedule.tsx
+++ src/app/schedule.tsx
@@ -1,5 +1,5 @@
-import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { addDays } from "date-fns";
+import { ChevronRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
@@ -7,8 +7,42 @@ import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const lessonIds = Array.from({ length: 10 }, (_, i) => i + 1);
-
+const lessonIds = [
+  { id: 1, img_path: "/assets/lesson-pic-1.png", desc: "Get to know your car" },
+  { id: 2, img_path: "/assets/lesson-pic-2.png", desc: "Balancing the pedals" },
+  {
+    id: 3,
+    img_path: "/assets/lesson-pic-3.png",
+    desc: "Gearbox & Steering Control",
+  },
+  { id: 4, img_path: "/assets/lesson-pic-4.png", desc: "Conquering Parking" },
+  {
+    id: 5,
+    img_path: "/assets/lesson-pic-5.png",
+    desc: "Driving at steady speed ",
+  },
+  {
+    id: 6,
+    img_path: "/assets/lesson-pic-6.png",
+    desc: "Hitting the main road",
+  },
+  {
+    id: 7,
+    img_path: "/assets/lesson-pic-7.png",
+    desc: "Bumper to bumper traffic",
+  },
+  { id: 8, img_path: "/assets/lesson-pic-8.png", desc: "Evening driving" },
+  {
+    id: 9,
+    img_path: "/assets/lesson-pic-9.png",
+    desc: "Comfortable with flyovers",
+  },
+  {
+    id: 10,
+    img_path: "/assets/lesson-pic-10.png",
+    desc: "Mini challenges -  Test Prep",
+  },
+];
 export default function Schedule() {
   return (
     <div className="flex h-full w-full p-6 pb-20">
@@ -37,7 +71,7 @@ export default function Schedule() {
               />
             </CardContent>
           </Card>
-          <Card className="mt-auto">
+          <Card className="mt-6">
             <CardContent className="flex h-full items-center justify-between gap-2 py-4">
               <p className="flex h-full flex-col justify-center gap-1 text-sm">
                 <span className="text-accent-purple">Lesson 1</span>
@@ -51,27 +85,41 @@ export default function Schedule() {
           </Card>
         </TabsContent>
         <TabsContent value="lesson" className="flex h-full flex-col">
-          <div className="grow">
-            <ScrollArea className="overflow-y-auto">
-              <div className="flex h-full w-full flex-col gap-2">
+          <div className="h-full grow">
+            <div className="flex h-[95%] w-full flex-col gap-2 overflow-y-auto">
+              <div className="grid grid-cols-2 gap-4">
                 {lessonIds.map((lesson) => (
-                  <Card key={lesson}>
-                    <CardContent className="flex h-full items-center justify-between gap-2 py-4">
-                      <p className="flex h-full flex-col justify-center gap-1 text-sm">
-                        <span className="text-accent-purple">
-                          Lesson {lesson}
-                        </span>
-                        <span>10th Sept</span>
-                        <span>9:00 AM</span>
-                      </p>
-                      <Button variant={"secondary"} asChild>
-                        <Link to="/lesson/1">Go to lesson</Link>
-                      </Button>
-                    </CardContent>
-                  </Card>
+                  <div
+                    key={lesson.id}
+                    className="flex flex-col gap-1 rounded-md bg-gray-50 p-3 shadow-md"
+                  >
+                    <p className="text-accent-purple">Lesson {lesson.id}</p>
+                    <div className="relative">
+                      <img src={lesson.img_path} alt="Lesson-pic" />
+                      <div className="absolute -bottom-1.5 right-1 flex w-[75%] flex-row items-center justify-center gap-1 rounded-sm bg-white px-1.5 py-1 shadow-md">
+                        <Link
+                          to={`/lesson/${lesson.id}`}
+                          className="text-xs text-primary"
+                        >
+                          More details
+                        </Link>
+                        <ChevronRight
+                          color="white"
+                          className="rounded-full bg-primary"
+                          size={16}
+                        />
+                      </div>
+                    </div>
+                    <Link
+                      to={`/lesson/${lesson.id}`}
+                      className="text-md mt-1.5"
+                    >
+                      {lesson.desc}
+                    </Link>
+                  </div>
                 ))}
               </div>
-            </ScrollArea>
+            </div>
           </div>
         </TabsContent>
       </Tabs>
