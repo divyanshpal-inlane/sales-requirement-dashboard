51d4cba584dadf698554784e4fb46c9877c23d03
nsys <nsys11@outlook.com>
2025-07-18 16:49:16 +0530

    Revert tree for PG integration

--- src/queries/learner.ts
+++ src/queries/learner.ts
@@ -211,7 +211,7 @@ export function useUploadLLMutation() {
       fileName?: string;
     }) => {
       const { data, error } = await supabase.storage
-        .from("ll")
+        .from("LL")
         .upload(`${phone}/${fileName}.${file.type.split("/")[1]}`, file, {
           cacheControl: "3600",
           upsert: true,
@@ -410,7 +410,7 @@ export function useLearnerEnrollmentCourse({
             .from("enrollment")
             .select("*, Courses(*)")
             .eq("learner_id", learnerId)
-            .eq("status", "active");
+            .in("status", ["pending", "active"]);
 
           if (error) throw error;
           return data;
