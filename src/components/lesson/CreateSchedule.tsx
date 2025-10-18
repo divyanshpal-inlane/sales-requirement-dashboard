43bd369d05936bc0282be1d610df4f13f4299169
Nikhilesh <nikhilesh@inlane.in>
2025-10-18 15:32:57 +0530

    Revert "Admin: check retry count"
    This reverts commit 2d48c3796004e1f620b682134cfbb1cdf2146b14.

--- src/components/lesson/CreateSchedule.tsx
+++ src/components/lesson/CreateSchedule.tsx
@@ -153,7 +153,6 @@ export async function getDrivingDistanceViaSDK(
           travelMode: google.maps.TravelMode.DRIVING,
         },
         (response, status) => {
-          // console.log("DistanceMatrix response, status:", response, status);
           if (
             status === "OK" &&
             response?.rows?.[0]?.elements?.[0]?.status === "OK"
@@ -342,20 +341,13 @@ export default function CreateScheduleWithInstructor({
 
           // Only fetch driving distance if straight-line distance is within a reasonable range
           // (e.g., 1.5x the instructor's radius) to save API calls
-          let drivingDistance: number | null = -1;
-          const maxRetryDistanceAPICallCount = 10;
+          let drivingDistance: number | null = null;
+          const maxRetryDistanceAPICallCount = 1000;
           let retryDistanceAPICallCount = 0;
 
           while (!drivingDistance && (retryDistanceAPICallCount < maxRetryDistanceAPICallCount)) {
             try {
-              console.log(
-                "Call distance API retry: ",
-                retryDistanceAPICallCount,
-                learnerLat,
-                learnerLng,
-                instructor.latitude,
-                instructor.longitude,
-              );
+              console.log("Call distance API retry: ", retryDistanceAPICallCount)
               drivingDistance = await getDrivingDistanceViaSDK(
                 learnerLat,
                 learnerLng,
@@ -373,7 +365,7 @@ export default function CreateScheduleWithInstructor({
             }
           }
 
-          if (retryDistanceAPICallCount > maxRetryDistanceAPICallCount) {
+          if (!drivingDistance) {
             console.error(
               "Distance API failed after " +
                 maxRetryDistanceAPICallCount +
