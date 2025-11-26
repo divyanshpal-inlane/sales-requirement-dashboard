ae4f0669586e2bed6c7e13f12bc97e5bf10f244d
Nikhilesh <nikhilesh@inlane.in>
2025-11-26 18:33:35 +0530

    Instr: change prev schedule lesson window

--- src/queries/instructor.ts
+++ src/queries/instructor.ts
@@ -174,8 +174,8 @@ export const useInstructorScheduleData = (phone: string) => {
   return useQuery({
     queryKey: ["instructor", phone],
     queryFn: async () => {
-      const maxInstrScheduleWindow = 7;
-      const startDate = subDays(new Date(), maxInstrScheduleWindow);
+      const maxInstrScheduleWindow = 15;
+      const startDate = subDays(new Date(), 2);
       const endDate = addDays(new Date(), maxInstrScheduleWindow);
       const currentDate = getCurrentDate();
 
